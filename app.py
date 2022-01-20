d7986f435580753d81110ccdb5fd4676f0f3239b
unknown <zanferencak@gmail.com>
2022-01-20 16:05:39 +0100

    Commiting changes before departure. Added some key functionality, started working on Python logic. Todo in app.py

--- app.py
+++ app.py
@@ -4,12 +4,33 @@ from datetime import datetime
 #
 eel.init('web')
 #
-
 @eel.expose
 def parseInput(args):
     print(args)
-    print(type(args))
+    eel.callResult(30)
+@eel.expose
+#TODO: MOVE LOGIC COUNTER AND STORAGE TO JS
+#      PASS LOGIC COUNTER AND STORAGE TO PYTHON THROUGH JS
+#      AND RETURN LOGIC RESULT BACK TO JS TO DISPLAY
+def logicParse(args):
+    operators = ['+','-','/','*']
+    try:
+        print(logicCounter)
+    except:
+        print("Give me my shares back, fucko")
+    print("Passing args ",args)
+    if(args in operators):
+        print("Operator detected")
+        logicCounter+=1
+        logicStorage[logicCounter]=args
+        logicCounter+=1
+    else:
+        print("Appending")
+        logicStorage[logicCounter] = logicStorage[logicCounter]+args
+        print(logicStorage)
 
-
+#vars
+logicStorage = []
+logicCounter = 0
 #
 eel.start('index.html')
--- web/js/main.js
+++ web/js/main.js
@@ -20,16 +20,30 @@ for (var i = 0; i < buttons ; i++) {
 function append(text){
   inputs.value = inputs.value + text;
 }
+function update(text){
+  inputs.value = text;
+}
+// flags
+var result = 0;
 
 function interpret(input){
-  console.log(input);
-
+  //console.log(input);
+  if(result==1){update(""); result=0;}
   switch(input){
-    case "C": inputs.value = ""; break;
-    case "DEL": inputs.value = (inputs.value).slice(0,-1); break;
+    case "C": update(""); break;
+    case "DEL": update((inputs.value).slice(0,-1)); break;
     case "x<sup>2</sup>": append("^2"); break;
     case "trig.": var trig = prompt("sin cos tan cotan",""); append(trig); break;
-    case "=": eel.parseInput(inputs.value); break;
-    default: append(input); break;
+    case "=": eel.parseInput(inputs.value); result = 1; break;
+    default: append(input); eel.logicParse(input); break;
   }
+}
+
+eel.expose(callResult);
+function callResult(args){
+  update(args);
+}
+eel.expose(jsLog);
+function jsLog(args){
+  console.log(args);
 }
