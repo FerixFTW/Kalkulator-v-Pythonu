57a5bf9cd0c3e65bbf4bd38b328f892678c3da40
ferixftw <zanferencak@gmail.com>
2022-03-03 21:13:26 +0100

    Refactored logic (heavily!), moved trig logic to separate file, made it more accurate

--- calLogic.py
+++ calLogic.py
@@ -1,21 +1,12 @@
 #imports
 import math
+import trig
 #####   INTERPRET
 #vars
 operators = ["+","-","*",'/']
 #def
 def interpret(args,ans):
-    temp_storage = ['']*32
-    index = 0
-    result = list(args)
-    #parse all operators and slot them in individual indexes
-    for element in result:
-        if(element in operators):
-            index = index + 1
-            temp_storage[index] = element
-            index = index + 1
-        else:
-            temp_storage[index] = temp_storage[index]+element
+    temp_storage = list(args)
     #parse all exceptions: exponent, sqrt, logarithms and trig functions
     index = 0
     for element in temp_storage:
@@ -25,6 +16,9 @@ def interpret(args,ans):
             if(sqrt_index!=0): #check factor before sqrt
                 factor=float(element[:sqrt_index])
             temp_storage[index] = factor * math.sqrt(float(element[(sqrt_index+1):]))
+        elif("-" in element and index == 0): #check if first element is negative
+            temp_storage[index+1] = float(temp_storage[index+1])*-1
+            temp_storage.pop(index)
         elif("^" in element): #parse exponent
             exp_index = element.index("^")
             base = float(element[:exp_index])
@@ -37,31 +31,32 @@ def interpret(args,ans):
             temp_storage[index] = factor * math.log10(float(element[(log_index+3):]))
         elif("sin" in element): #parse trig functions
             sin_index = element.index("sin")
+            value = float(element[(sin_index+3):])
             if(sin_index != 0):
                 factor = float(element[:sin_index])
-            if((float(element[(sin_index+3):])/90)%2==0):
-                temp_storage[index] = 0
-            else:
-                temp_storage[index] = factor * math.sin(math.radians(float(element[(sin_index+3):])))
+            temp_storage[index] = factor * trig.sin(value)
         elif("cos" in element):
             cos_index = element.index("cos")
+            value = float(element[(cos_index+3):])
             if(cos_index != 0):
                 factor = float(element[:cos_index])
-            if((float(element[(cos_index+3):])/90)%2!=0):
-                temp_storage[index] = 0
-            else:
-                temp_storage[index] = factor * math.cos(math.radians(float(element[(cos_index+3):])))
+            temp_storage[index] = factor * trig.cos(value)
         elif("tan" in element):
             tan_index = element.index("tan")
+            value = float(element[(tan_index+3):])
             if(tan_index != 0):
                 factor = float(element[:tan_index])
-            tan_value = element[(tan_index+3):]
-            temp_storage[index] = factor * math.tan(math.radians(float(element[(tan_index+3):])))
+            if(trig.tan(value) == "undefined"):
+                return "undefined"
+            temp_storage[index] = factor * trig.tan(value)
         elif("cot" in element):
             cot_index = element.index("cot")
+            value = float(element[(cot_index+3):])
             if(cot_index != 0):
                 factor = float(element[:cot_index])
-            temp_storage[index] = factor * math.cos(math.radians(float(element[(cot_index+3):])))/math.sin(math.radians(float(element[(cot_index+3):])))
+            if(trig.cot(value) == "undefined"):
+                return "undefined"
+            temp_storage[index] = factor * trig.cot(value)
         elif("ans" in element):
             ans_index = element.index("ans")
             if(ans_index != 0):
@@ -72,12 +67,7 @@ def interpret(args,ans):
     return solve(temp_storage,ans)
 
 #####   SOLVE
-#vars
-#def
 def solve(temp_storage,ans):
-    #strip blank space from temp_storage and add blank space to end for end of expression
-    temp_storage[:] = (value for value in temp_storage if value != '')
-    temp_storage.append('')
     #first solve multiplication and division
     index = 0
     for element in temp_storage:
@@ -102,8 +92,6 @@ def solve(temp_storage,ans):
     return val_storage
 
 ##### log
-#vars
-#def
 def log(args):
     for element in args:
         print(">",element, end =" ")
---
+++ trig.py
@@ -0,0 +1,47 @@
+#####trig.py
+# Return accurate results from trig functions
+# Takes degrees values by default
+import math
+#
+# TODO: 0.5 and -0.5 values for all functions if I want true precision
+def sin(args):
+    args = float(args) #redundancy float conversion
+    if(args%180==0):
+        return float(0)
+    else:
+        print("sin ",math.sin(math.radians(args)))
+        return math.sin(math.radians(args))
+#
+def cos(args):
+    args = float(args) #redundancy float conversion
+    if(((args*2-180)/360).is_integer()):
+        return float(0)
+    else:
+        print("cos ",math.cos(math.radians(args)))
+        return math.cos(math.radians(args))
+#
+def tan(args):
+    args = float(args) #redundancy float conversion
+    if(args%180==0):
+        return float(0)
+    elif(((args*2-180)/360).is_integer()):
+        return "undefined"
+    elif(((4*args-180)/720).is_integer()):
+        return float(1)
+    elif(((4*args-3*180)/720).is_integer()):
+        return float(-1)
+    else:
+        return math.tan(math.radians(args))
+#
+def cot(args):
+    args = float(args) #redundancy float conversion
+    if(((args*2-180)/360).is_integer()):
+        return float(0)
+    elif(args%180==0):
+        return "undefined"
+    elif((4*args-180)/720).is_integer():
+        return float(1)
+    elif((4*args-3*180)/720).is_integer():
+        return float(-1)
+    else:
+        return float(cos(args)/sin(args))
