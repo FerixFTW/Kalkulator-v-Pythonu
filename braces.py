febb47af470aeaa5fc472b71bee7ed638e2a90c2
ferixftw <zanferencak@gmail.com>
2023-01-13 22:03:38 +0100

    Declared check_braces() callable fnc

--- braces.py
+++ braces.py
@@ -1,73 +1,57 @@
 # braces.py
 ## check if string contains braces
-## if does, get the contents in between braces
+## if does, interpret the contents in between braces
 
 import calLogic as logic
 
-string = "5 * ( 3 + 4 )"
+def check_braces(string):
 
-print(string)
+    start_braces = string.count('(')
+    end_braces = string.count(')')
 
-#string.find - find index of the first occurence of character
-#string.count - count occurences of character in string
+    if(start_braces != end_braces):
+        print("Unmatched braces!")
+        return "err"
 
-start_braces = string.count('(')
-end_braces = string.count(')')
+    # iterate string and find all open and close braces
 
-if(start_braces != end_braces):
-    print("Unmatched braces!")
-    exit()
+    open_braces = []
+    close_braces = []
 
-# now lets get the content between the braces
+    for index, letter in enumerate(string):
+        if letter == '(':
+            open_braces.append(index)
+        if letter == ')':
+            close_braces.append(index)
 
-start_brace = string.find('(')
-end_brace = string.find(')')
+    # check distances between indexes
 
-contents = string[start_brace+1:end_brace]
+    for i in range(len(open_braces)):
+        for index in range(len(open_braces)-1):
+            distance1 = close_braces[index] - open_braces[index]
+            distance2 = close_braces[index] - open_braces[index+1]
 
-# print(contents)
+            if( distance2 < distance1 and distance2 > 0 ):
+                store = open_braces[index] # store current idx
+                open_braces[index] = open_braces[index+1] #
+                open_braces[index+1] = store
 
-# iterate string and find all open and close braces
+    # print the contents between the braces
 
-open_braces = []
-close_braces = []
+    brace_index = 0
 
-for index, letter in enumerate(string):
-    if letter == '(':
-        open_braces.append(index)
-    if letter == ')':
-        close_braces.append(index)
+    for index in range(len(open_braces)):
+        contents = string[open_braces[index]+1:close_braces[index]].replace("(","").replace(")","")
+        solved_contents = logic.interpret(contents,0)
+        string = string.replace(contents,str(solved_contents))
 
-print(open_braces)
-print(close_braces)
+    parsed_string = string.replace("(","").replace(")","")
+    result = logic.interpret(parsed_string)
 
-# check distances between indexes
+    return result
 
-for i in range(6):
-    for index in range(len(open_braces)-1):
-        distance1 = close_braces[index] - open_braces[index]
-        distance2 = close_braces[index] - open_braces[index+1]
-        # print("\nIndex ", index)
-        # print("Comparing: ", distance1, " ", distance2)
-        if( distance2 < distance1 and distance2 > 0 ):
-            print("Switching")
-            store = open_braces[index] # store current idx
-            open_braces[index] = open_braces[index+1] #
-            open_braces[index+1] = store
+#
 
-print("Fixed status:")
-print(open_braces)
-print(close_braces)
-print("\n")
-
-# print the contents between the braces
-
-brace_index = 0
-
-for index in range(len(open_braces)):
-    contents = string[open_braces[index]+1:close_braces[index]].replace("(","").replace(")","")
-    solved_contents = logic.interpret(contents,0)
-    string = string.replace(contents,str(solved_contents))
-
-print(string.replace("(","").replace(")",""))
-print(logic.interpret(string.replace("(","").replace(")",""),0))
+if __name__ == '__main__':
+    string = "5 * ( 3 + 4 )"
+    check_braces(string)
