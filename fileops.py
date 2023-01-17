fec70ff4427845ff747c530aadf813c8acbe774b
unknown <zanferencak@gmail.com>
2023-01-17 19:11:06 +0100

    Added ans functionality to files

--- fileops.py
+++ fileops.py
@@ -6,9 +6,11 @@ import calLogic as logic
 
 def parseFile(file):
     results = []
+    last_ans = 0
     with open(file,"r") as read_file:
         for line in read_file:
-            result = logic.interpret(line.replace("=","").rstrip())
+            result = logic.interpret(line.replace("=","").rstrip(),last_ans)
+            last_ans = result
             results.append(result)
 
     return results
